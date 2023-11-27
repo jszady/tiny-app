584ea8c96dea753e3b107bc11891427b4c464285
jszady <jszady99@gmail.com>
2023-11-27 18:19:21 -0500

    finished the nav and the urls

---
+++ express_server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+
+app.set('view engine', 'ejs');
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+}
+
+
+app.get('/urls.json', (req, res) => {
+  res.send(urlDatabase);
+})
+
+
+app.get('/urls', (req, res) => {
+  
+  const templateVars = {
+    urls: urlDatabase
+  }
+  
+  res.render('urls_index', templateVars);
+ 
+})
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  res.render("urls_show", templateVars);
+});
+
+app.get('/', (req, res) => {
+  res.send("Hello");
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening on PORT: ${PORT}`);
+})
