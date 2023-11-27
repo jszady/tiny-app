f501cd92bfb664a89692e28f776e4f205e83e4c8
jszady <jszady99@gmail.com>
2023-11-27 18:34:21 -0500

    let user POST a new url to the console

--- express_server.js
+++ express_server.js
@@ -4,6 +4,7 @@ const PORT = 8080;
 
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -15,6 +16,10 @@ app.get('/urls.json', (req, res) => {
   res.send(urlDatabase);
 })
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 app.get('/urls', (req, res) => {
   
@@ -26,10 +31,15 @@ app.get('/urls', (req, res) => {
  
 })
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+})
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
+
 
 app.get('/', (req, res) => {
   res.send("Hello");
@@ -38,3 +48,8 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Listening on PORT: ${PORT}`);
 })
+
+
+function generateRandomString() {
+  
+}
