3e491f54dfc7539a294a4104996d5d7164bde13f
jszady <jszady99@gmail.com>
2023-11-28 14:43:04 -0500

    created the delete post route (everything works)

--- express_server.js
+++ express_server.js
@@ -11,50 +11,87 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 }
 
+/*
 
-app.get('/urls.json', (req, res) => {
-  res.send(urlDatabase);
+
+All my GETS
+
+
+*/
+
+// this route will handle the page where users can add a new URL
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
 })
 
 
+//this route will diplay all the shortened URLs
 app.get('/urls', (req, res) => {
   
   const templateVars = {
     urls: urlDatabase
   }
   
-  res.render('urls_index', templateVars);
- 
+  //this will render the urls_index.ejs and show the list of URLs
+  res.render('urls_index', templateVars); 
 })
 
+
+// this redirecrs the short URL to its original URL  
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  console.log(longURL);
+  // redirects the user to the original URL 
+  res.redirect(longURL);
+});
+
+
+// this route will diplay a specific URL
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  // renders the urls_show.ejs and dispplays the URLS amd short form
+  res.render("urls_show", templateVars);
+});
+
+app.get('/', (req, res) => {
+  res.send("Hello");
+})
+
+app.get('/urls.json', (req, res) => {
+  res.send(urlDatabase);
+})
+/*
+
+
+ALL my POSTS
+
+
+*/
 app.post("/urls", (req, res) => {
   const id = generateRandomString();
-
+  
+  if(!req.body.longURL.includes('http://')){
+    req.body.longURL = 'http://' + req.body.longURL;
+  }
   // saves the random id and the longURL
   urlDatabase[id] = req.body.longURL;
-
+  console.log(urlDatabase);
   // this will redirect to the id urls:id
   res.redirect(`/urls/${id}`);
 
 });
 
-app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+// this will delete the url
+app.post('/urls/:id/delete', (req, res) => {
+  
+  const id = req.params.id;
+  
+  delete urlDatabase[id];
+
+  // we are going to stay on the same page but just delete the url 
+  res.redirect('/urls');
 })
-app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id];
-  res.redirect(longURL);
-});
 
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
-  res.render("urls_show", templateVars);
-});
-
-
-app.get('/', (req, res) => {
-  res.send("Hello");
-})
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT: ${PORT}`);
