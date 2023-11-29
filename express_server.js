14874bc18e881bcd13721d03286c31643d0964e0
jszady <jszady99@gmail.com>
2023-11-28 20:28:39 -0500

    created the cookie for username in features/cookie branch (everything works)

--- express_server.js
+++ express_server.js
@@ -101,6 +101,13 @@ app.post('/urls/:id', (req, res) => {
   res.redirect(`/urls/`);
 })
 
+app.post('/login', (req, res) => {
+
+  const username = req.body.username;
+  res.cookie('username', username);
+  res.redirect('/urls');
+})
+
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT: ${PORT}`);
